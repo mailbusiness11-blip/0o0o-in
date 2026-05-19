4ea873c5437030c07ddc544e3b7ad14424f9cb77
mailbusiness11-blip <mail.business11@gmail.com>
2026-05-19 21:13:38 +0530

    fix layout crash

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import "./globals.css";
-import Providers from "./providers";
+
+export const metadata = {
+  title: "0o0o",
+  description: "Store",
+};
 
 export default function RootLayout({
   children,
@@ -8,57 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <Providers>
-          <nav
-            style={{
-              background: "black",
-              padding: "20px 40px",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <a
-              href="/products"
-              style={{
-                color: "white",
-                fontSize: "28px",
-                fontWeight: "bold",
-              }}
-            >
-              0o0o Store
-            </a>
-
-            <div
-              style={{
-                display: "flex",
-                gap: "20px",
-              }}
-            >
-              <a
-                href="/products"
-                style={{
-                  color: "white",
-                }}
-              >
-                Products
-              </a>
-
-              <a
-                href="/cart"
-                style={{
-                  color: "white",
-                }}
-              >
-                Cart
-              </a>
-            </div>
-          </nav>
-
-          {children}
-        </Providers>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
